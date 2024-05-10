e953d05508fe23eb0cadecf59dedd729ea832000
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-05-10 20:59:38 +0300

    app controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,17 @@
-const { DBClient } = require('../utils/db');
-const { getRedisStatus } = require('../utils/redis');
+const  dbClient  = require('../utils/db');
+const  redisClient  = require('../utils/redis');
 
 const AppController = {
 	getStatus: (req, res) => {
-		const redisStatus = getRedisStatus();
-		const dbStatus = DBClient.isAlive();
+		const redisStatus = redisClient.isAlive();
+		const dbStatus = dbClient.isAlive();
 
 		res.status(200).json({ redis: redisStatus, db: dbStatus });
 	},
 
 	getStats: (req, res) => {
-		const userCount = DBClient.nbUsers();
-		const fileCount = DBClient.nnbFiles();
+		const userCount = dbClient.nbUsers();
+		const fileCount = dbClient.nnbFiles();
 
 		res.status(200).json({ users: userCount, files: fileCount });
 	},
