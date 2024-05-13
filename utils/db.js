76d634501b7a81c6fd1324e1715110a23345a572
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-05-13 14:23:46 +0300

    .db{}

--- utils/db.js
+++ utils/db.js
@@ -3,13 +3,13 @@ const { MongoClient } = require('mongodb');
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${host}:${port}/${database}`;
+const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true});
     this.client.connect().then(
-		this.db = this.client.db(database)
+		this.db = this.client.db(`${database}`)
 	).catch((error) => console.log(error))
   }
 
