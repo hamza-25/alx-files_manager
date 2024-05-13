a8692abdafa88b39e36dcbf651f6940af183658a
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-05-14 00:19:14 +0300

    await

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,23 +7,22 @@ const redisClient = require('../utils/redis');
 const AuthController = {
   getConnect: async (req, res) => {
     const token = req.header('Authorization').split(' ')[1];
+    if ( !token ){
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
     const email = Buffer.from(token, 'base64').toString().split(':')[0];
     const hashPassword = sha1(Buffer.from(token, 'base64').toString().split(':')[1]);
     const generateToken = uuidv4();
-    if ( !token ){  
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
     try {
       const user = await dbClient.db.collection('users').findOne({ email });
       if (!user) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-
       if (hashPassword !== user.password) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
       const key = `auth_${generateToken}`;
-      redisClient.set(key, user._id.toString(), (60 * 60 * 24));
+      await redisClient.set(key, user._id.toString(), (60 * 60 * 24));
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
