0c187f29f39eaa4e181b37ce0e46ee42bd65460b
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-05-13 22:09:51 +0300

    auth file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -188,18 +188,29 @@ const FilesController = {
   },
   getFile: async (req, res) => {
     const { id } = req.params;
+    const xToken = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${xToken}`);
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
     const file = await dbClient.db.collection('files').findOne({
       _id: ObjectId(id)
     });
-    if (!file || file.isPublic === false || !fs.existsSync(file.localPath)) {
+    if (!file  || !fs.existsSync(file.localPath)) {
       return res.status(404).json({ error: 'Not found' });
+    } else {
+      if (file.type === 'folder') {
+        return res.status(400).json({ error: "A folder doesn't have content" });
+      }
+      if (file.isPublic === false ) {
+        if (!user || file.userId !== user._id) {
+          return res.status(404).json({ error: 'Not found' });
+        } else {
+          const data = await fs.readFile(file.localPath)
+          const mimeType = mime.contentType(file.name);
+          return res.header('Content-Type', mimeType).status(200).send(data);
+        }
+       
+      }
     }
-    if (file.type === 'folder') {
-      return res.status(400).json({ error: 'A folder doesn\'t have content' });
-    }
-    const data = await fs.readFile(file.localPath)
-    const mimeType = mime.contentType(file.name);
-    return res.header('Content-Type', mimeType).status(200).send(data);
   },
 };
 
